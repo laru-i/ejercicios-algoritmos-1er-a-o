f775755a0db07af034a40ed0456820c01ca02709
unknown <27.lara.tartaglia@gmail.com>
2022-07-14 03:22:01 -0300

    hola

---
+++ ejautoevaluacion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define f 2
+#define c 2
+
+using namespace std;
+/*
+En la facultad se dictarán 15 cursos extraprogramáticos en el segundo semestre. Todos los cursos duran
+un mes y se dictará una edición por mes, esto quiere decir que se repite todos los meses.
+Primero se ingresarán los cursos, por cada curso se sabe:
+código de curso
+nombre del curso
+cupo para cada edición.
+
+A continuación se ingresan las inscripciones. Por cada inscripción se ingresa:
+código de curso
+mes en el que quiere realizar el curso (7 a 12)
+El ingreso de datos finaliza con código de curso cero.
+
+Si no hay cupo para ese curso en ese mes emitir el mensaje "inscripción rechazada por falta de cupo".
+Una vez finalizada la inscripción, el programa debe:
+1. Informar la cantidad de inscriptos por cada curso y por cada mes.
+2. Informar código de curso y cantidad total de inscriptos, ordenado de mayor a menor por
+cantidad total de inscriptos por curso (sin tener en cuenta los meses)
+*/
+
+int main()
+{
+    
+
+
+    return 0;
+}
