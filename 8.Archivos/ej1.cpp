a30850951d6ce63d9b059a1431321c21b75ac0bc
unknown <27.lara.tartaglia@gmail.com>
2022-08-03 20:47:36 -0300

    ej1

--- 8.Archivos/ej1.cpp
+++ 8.Archivos/ej1.cpp
@@ -16,6 +16,9 @@ a.3 Día del examen a.4 Mes del examen
 a.5 Año del examen a.6 Nombre-Apellido
 
 */
+
+void generarArchivo(Inscripcion inscri);
+
 struct Inscripcion
 {
     int leg, cod, dia, mes, ano;
@@ -25,57 +28,58 @@ struct Inscripcion
 int main()
 {
     Inscripcion inscri;
-    ingreso(inscri);
+    // ingreso(inscri);
     generarArchivo(inscri);
 
     return 0;
 }
-void ingreso(Inscripcion inscri)
-{
-    cout << "ingrese legajo, 0 para finalizar ";
-    cin >> inscri.leg;
+// void ingreso(Inscripcion inscri)
+// {
+//     cout << "ingrese legajo, 0 para finalizar ";
+//     cin >> inscri.leg;
 
-    while (inscri.leg != 0)
-    {
+//     while (inscri.leg != 0)
+//     {
 
-        cout << "ingrese nombre y apellido ";
-        cin >> inscri.nombre;
+//         cout << "ingrese nombre y apellido ";
+//         cin >> inscri.nombre;
 
-        cout << "ingrese codigo de materia ";
-        cin >> inscri.cod;
+//         cout << "ingrese codigo de materia ";
+//         cin >> inscri.cod;
 
-        cout << "ingrese dia, mes y ano ";
-        cin >> inscri.dia >> inscri.mes >> inscri.ano;
+//         cout << "ingrese dia, mes y ano ";
+//         cin >> inscri.dia >> inscri.mes >> inscri.ano;
 
-        cout << "ingrese legajo, 0 para finalizar ";
-        cin >> inscri.leg;
-    }
-}
+//         cout << "ingrese legajo, 0 para finalizar ";
+//         cin >> inscri.leg;
+//     }
+// }
 
 void generarArchivo(Inscripcion inscri)
 {
     FILE *archParciales;
-    char nomArch[21];
-    cout << "Nombre archivo (poner al final .dat)" << endl;
-    cin >> nomArch;
-    archParciales = fopen(nomArch, "wb");
-    // archParciales=fopen("Parcial1.dat","wb");
+    archParciales = fopen("DIAFINALES.dat", "wb");
     if (archParciales == NULL)
         cout << "ERROR" << endl;
     else
     {
-        Parcial par;
+        Inscripcion inscri;
         cout << "Ingrese legajo (0 para finalizar) ";
-        cin >> par.legajo;
-        while (par.legajo != 0)
+        cin >> inscri.leg;
+        while (inscri.leg != 0)
         {
-            cout << "Nombre: ";
-            cin >> par.nombre;
-            cout << "Nota: ";
-            cin >> par.nota;
-            fwrite(&par, sizeof(Parcial), 1, archParciales);
+            cout << "ingrese nombre y apellido ";
+            cin >> inscri.nombre;
+
+            cout << "ingrese codigo de materia ";
+            cin >> inscri.cod;
+
+            cout << "ingrese dia, mes y ano ";
+            cin >> inscri.dia >> inscri.mes >> inscri.ano;
+
+            fwrite(&inscri, sizeof(Inscripcion), 1, archParciales);
             cout << "Ingrese legajo (0 para finalizar) ";
-            cin >> par.legajo;
+            cin >> inscri.leg;
         }
         fclose(archParciales);
     }
