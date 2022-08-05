c7d67a641af0552272141e97dc308e4ba265c67e
unknown <27.lara.tartaglia@gmail.com>
2022-08-05 18:40:38 -0300

    ej3archivos

--- 8.Archivos/ej3.cpp
+++ 8.Archivos/ej3.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 /*
 
-Dado el archivo binario generado en el ejercicio 1, que contiene todas las inscripciones del día, y otro con el mismo 
-diseño que contiene las inscripciones anteriores (FINALES.DAT), desarrolle un programa que agregue al archivo de 
+Dado el archivo binario generado en el ejercicio 1, que contiene todas las inscripciones del día, y otro con el mismo
+diseño que contiene las inscripciones anteriores (FINALES.DAT), desarrolle un programa que agregue al archivo de
 inscripciones anteriores el contenido del archivo del día.
 
 */
-void generarArchivo(Inscripcion inscri);
-
 struct Inscripcion
 {
     int leg, cod, dia, mes, ano;
@@ -21,94 +19,26 @@ struct Inscripcion
 int main()
 {
     Inscripcion inscri;
-    generarArchivo(inscri);
-
-    FILE *ae = fopen("DIAFINALES.dat", "rb");
-    if (ae == NULL)
+    FILE *archFin = fopen("FINALES.dat", "ab");
+    if (archFin == NULL)
         cout << "ERROR" << endl;
     else
     {
-        fread(&inscri, sizeof(Inscripcion), 1, ae);
-        while (!feof(ae))
+        FILE *archDias = fopen("DIAFINALES.dat", "rb");
+        if (archDias == NULL)
+            cout << "ERROR" << endl;
+        else
         {
-            cout << inscri.leg << " " << inscri.cod << " " << inscri.dia << "/" << inscri.mes << "/" << inscri.ano << " " << inscri.nombre << " " << inscri.apellido << endl;
-            fread(&inscri, sizeof(Inscripcion), 1, ae);
+            fread(&inscri, sizeof(Inscripcion), 1, archDias);
+            while (!feof(archDias))
+            {
+                fwrite(&inscri, sizeof(Inscripcion), 1, archFin);
+
+                fread(&inscri, sizeof(Inscripcion), 1, archDias);
+            }
         }
-        fclose(ae);
     }
+    fclose(archFin);
 
     return 0;
-}
-
-void generarArchivo()
-{
-    FILE *archParciales;
-    archParciales = fopen("DIAFINALES.dat", "wb");
-    if (archParciales == NULL)
-        cout << "ERROR" << endl;
-    else
-    {
-        Inscripcion inscri;
-        cout << "Ingrese legajo (0 para finalizar) ";
-        cin >> inscri.leg;
-        while (inscri.leg != 0)
-        {
-            cout << "ingrese nombre y apellido ";
-            cin >> inscri.nombre >> inscri.apellido;
-            // strcat(inscri.nombre, inscri.apellido);
-
-            cout << "ingrese codigo de materia ";
-            cin >> inscri.cod;
-
-            cout << "ingrese dia (numericamente) ";
-            cin >> inscri.dia;
-            if (inscri.dia < 1 || inscri.dia > 31)
-            {
-                cout << "error, ingrese dia nuevamente ";
-                cin >> inscri.dia;
-            }
-
-            cout << "ingrese mes (numericamente) ";
-            cin >> inscri.mes;
-            if (inscri.mes < 1 || inscri.mes > 12)
-            {
-                cout << "error, ingrese mes nuevamente ";
-                cin >> inscri.mes;
-            }
-
-            cout << "ingrese ano (numericamente) ";
-            cin >> inscri.ano;
-
-            fwrite(&inscri, sizeof(Inscripcion), 1, archParciales);
-
-            cout << "Ingrese legajo (0 para finalizar) ";
-            cin >> inscri.leg;
-        }
-        fclose(archParciales);
-    }
-}
-
-void ingreso()
-{
-    int codMateria;
-
-    cout << "ingrese codigo de materia, 0 para finalizar " << endl;
-    cin >> codMateria;
-
-    while (codMateria != 0)
-    {
-        cout << "ingrese codigo de materia, 0 para finalizar " << endl;
-        cin >> codMateria;
-    }
-
-    for (int i = 0; i < ; i++)
-    {
-        if (codMateria == inscri.cod)
-        {
-            
-        }
-        
-    }
-    
-    
 }
