671c747764fc8b2edfad93caebab9d890d00f2a7
unknown <27.lara.tartaglia@gmail.com>
2022-08-21 23:59:53 -0300

    act

---
+++ 8.Archivos/ej4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+/*
+Dado un archivo binario de productos que contiene 100 registros, y cada registro corresponde a un producto que está 
+codificado del 1 a 100, ordenado por código de producto con el siguiente diseño: 
+    código de producto (1..100) stock 
+y otro archivo binario de pedidos, con el siguiente diseño:
+    número de pedido número de cliente 
+    código de producto (1..100) cantidad pedida 
+Se pide desarrollar un algoritmo que:
+
+a) Realice un listado con los pedidos que fueron satisfechos en su totalidad según el siguiente listado
+Pedidos Satisfechos
+Número de pedido Número de Cliente Código de Producto Cantidad pedida 
+9999 999999 999 9999
+9999 999999 999 9999
+
+b) actualizar el campo stock del archivo de producto, cada vez que acepte un pedido.
+
+c) grabar un archivo binario con el mismo diseño que el archivo de pedidos, con aquellos pedidos que no 
+pueden ser satisfechos en su totalidad.
+*/
+
+int main()
+{
+
+
+
+
+    
+}
--- ejautoevaluacion.cpp
+++ ejautoevaluacion.cpp
@@ -1,32 +1,126 @@
 #include <iostream>
-#define f 2
-#define c 2
 
 using namespace std;
-/*
-En la facultad se dictarán 15 cursos extraprogramáticos en el segundo semestre. Todos los cursos duran
-un mes y se dictará una edición por mes, esto quiere decir que se repite todos los meses.
-Primero se ingresarán los cursos, por cada curso se sabe:
-código de curso
-nombre del curso
-cupo para cada edición.
 
-A continuación se ingresan las inscripciones. Por cada inscripción se ingresa:
-código de curso
-mes en el que quiere realizar el curso (7 a 12)
-El ingreso de datos finaliza con código de curso cero.
 
-Si no hay cupo para ese curso en ese mes emitir el mensaje "inscripción rechazada por falta de cupo".
-Una vez finalizada la inscripción, el programa debe:
-1. Informar la cantidad de inscriptos por cada curso y por cada mes.
-2. Informar código de curso y cantidad total de inscriptos, ordenado de mayor a menor por
-cantidad total de inscriptos por curso (sin tener en cuenta los meses)
-*/
+struct Curso
+{
+    int cod;
+    string nombre;
+    int cupo;
+};
+//prototipi de funciones
+void inicializar(int [][6],int,int);//inicializa la matriz en cero
+void sumaFilas(int m[][6],int cf,int cc,int vs[]);
+void ordenar(int vt[],Curso vc[],int t);//ordena vt de > a < arrastrando vc
+void ingresarCursos(Curso v[],int c);
+void ingresarInscripciones(Curso vc[],int m[][6],int cc);
+int buscar(Curso v[],int t,int unCod);
+void punto1(int m[][6],int cf,int cc,Curso v[]);
+void punto2(int vt[],int c,Curso vc[]);
 
 int main()
 {
-    
+ //declarar variables
+ Curso vcursos[15];
+ int matInsc[15][6],totCurso[15];
 
+ inicializar(matInsc,15,6);
+ ingresarCursos(vcursos,15); //agregar argumentos
+ ingresarInscripciones(vcursos,matInsc,15);//agregar argumentos
+ punto1(matInsc,15,6,vcursos); //agregar argumentos
+ sumaFilas(matInsc,15,6,totCurso);
+ ordenar(totCurso,vcursos,15);
+ punto2(totCurso,15,vcursos); //agregar argumentos
 
-    return 0;
+ return 0;
 }
+//funciones
+void ingresarCursos(Curso v[],int c)
+{
+    for(int i=0;i<c;i++)
+    {
+        do
+            cin>>v[i].cod;
+        while(v[i].cod==0);
+        cin>>v[i].nombre;
+        cin>>v[i].cupo;
+    }
+}
+void ingresarInscripciones(Curso vc[],int m[][6],int cc)
+{
+    int curso,mes,pos;
+    cin>>curso;
+    while(curso!=0)
+    {
+        pos=buscar(vc,cc,curso);
+        if(pos!=-1)
+        {
+            do
+                cin>>mes;
+            while(mes<7||mes>12);
+            if(m[pos][mes-7]<vc[pos].cupo)
+                m[pos][mes-7]++;
+            else
+                cout<<"inscripci�n rechazada por falta de cupo"<<endl;
+        }
+        cin>>curso;
+    }
+}
+int buscar(Curso v[],int t,int unCod)
+{
+    int i=0;
+    while(i<t && v[i].cod!=unCod)
+        i++;
+    if(i==t)
+        return -1;
+    else
+        return i;
+}
+void punto1(int m[][6],int cf,int cc,Curso v[])
+{
+    for(int i=0;i<cf;i++)
+    {
+        cout<<"Curso "<<v[i].cod<<v[i].nombre<<endl;
+        for(int j=0;j<cc;j++)
+        {
+            cout<<"Mes "<<j+7<<" inscriptos "<<m[i][j]<<endl;
+        }
+    }
+}
+void sumaFilas(int m[][6],int cf,int cc,int vs[])
+{
+    for(int i=0;i<cf;i++)
+    {
+        vs[i]=0;
+        for(int j=0;j<cc;j++)
+            vs[i]+=m[i][j];
+    }
+}
+void punto2(int vt[],int c,Curso vc[])
+{
+    for(int i=0;i<c;i++)
+        cout<<vc[i].cod<<vt[i]<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
