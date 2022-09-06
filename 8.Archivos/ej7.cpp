6f4c0267d7ae319bd8caf47c25c75e1c6bb9a373
unknown <27.lara.tartaglia@gmail.com>
2022-09-05 21:54:30 -0300

    ej7

---
+++ 8.Archivos/ej7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+/*
+En un comercio se tiene el archivo “VentasJunio.dat” con un registro por cada uno de los 30 días del mes de junio con 
+las ventas acumuladas de ese día. Cada registro tiene día, importe total y cantidad de ventas. El archivo está ordenado 
+por día. Hacer un programa que informe el importe de las ventas del día 15 y además que informe cuándo hubo más 
+ventas, si el primero o el último día del mes
+*/
+
+int main(){
+
+
+
+    return 0;
+}
